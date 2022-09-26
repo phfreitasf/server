41721e89529fddb3864fa1a542256017e1adb8b0
Pedro Henrique <31445164+phfreitasf@users.noreply.github.com>
2022-09-26 20:38:25 -0300

    Delete build directory

--- build/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const appExpress_1 = require("./serverConfig/appExpress");
-const https_1 = require("./serverConfig/https");
-const http_1 = require("./serverConfig/http");
-http_1.httpServer.listen(appExpress_1.httpPort);
-https_1.httpsServer.listen(appExpress_1.httpsPort);
-console.log('Servidor http rodando na porta : ', appExpress_1.httpPort);
-console.log('Servidor https rodando na porta : ', appExpress_1.httpsPort);
